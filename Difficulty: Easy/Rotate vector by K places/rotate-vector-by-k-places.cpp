4bbafd9058e04257e9d32a8159e511d92d5b93b8
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-04 20:39:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate vector by K places/rotate-vector-by-k-places.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void reverse(vector<int> &arr, int l, int r) {
+        while (l < r) {
+            swap(arr[l], arr[r]);
+            l++;
+            r--;
+        }
+    }
+
+    void leftRotate(vector<int>& arr, int k) {
+        int n = arr.size();
+        k %= n;
+
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+};

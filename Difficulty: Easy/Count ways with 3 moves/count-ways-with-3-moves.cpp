4768ffeb558879970d963e2b4b455223d49e119b
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-06 22:51:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    int countWays(int n) {
+        // your code here
+          if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+};

5463791746f729b415c45babad4003242342dd5d
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-13 00:49:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Steps to Make 1/minimum-steps-to-make-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int getMinSteps(int n) {
+        // code here
+        vector<int>dp(n+1);
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            dp[i]=1+dp[i-1];
+            if(i%2==0){
+             dp[i]= min(dp[i],1+dp[i/2]);
+            }
+            if(i%3==0){
+                dp[i]= min(dp[i],1+dp[i/3]);
+            }
+        }
+        return dp[n];
+    }
+};

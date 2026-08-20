e24b909cc7a361e7a953d8182e53b10e66f3ce76
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-21 01:31:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum distance in an Array/minimum-distance-in-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int minDist(vector<int>& arr, int x, int y) {
+
+        int lastX = -1;
+        int lastY = -1;
+        int ans = INT_MAX;
+
+        for(int i = 0; i < arr.size(); i++) {
+
+            if(arr[i] == x) {
+                lastX = i;
+            }
+
+            if(arr[i] == y) {
+                lastY = i;
+            }
+
+            // Both have been found
+            if(lastX != -1 && lastY != -1) {
+                ans = min(ans, abs(lastX - lastY));
+            }
+        }
+
+        if(ans == INT_MAX)
+            return -1;
+
+        return ans;
+    }
+};

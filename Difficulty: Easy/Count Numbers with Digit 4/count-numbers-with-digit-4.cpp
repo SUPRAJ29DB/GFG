54ee8da50fb5957be0cb0df74edcd44b8319baa2
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-27 19:57:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Numbers with Digit 4/count-numbers-with-digit-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        // code here
+        int count=0;
+        for(int i=0;i<=n;i++){
+            int x=i;
+            while(x>0){
+                if(x%10==4){
+                    count++;
+                    break;
+                }
+                x/=10;
+            }
+        }
+        return count;
+    }
+};

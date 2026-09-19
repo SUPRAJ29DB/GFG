2b7a53e6c0a00c1f6c004b34ba78eed07ed91330
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-09-19 21:08:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairwise Swap in Linked List/pairwise-swap-in-linked-list.cpp
@@ -0,0 +1,40 @@
+/* Structure of linked list Node
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};*/
+class Solution {
+  public:
+    Node* pairwiseSwap(Node* head) {
+        // The task is to complete this method
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        Node * prev=NULL;
+        Node * first=head;
+        
+        while(first!=NULL && first->next!=NULL){
+            Node* second=first->next;
+            Node *nextpair=second->next;
+            
+            second->next=first;
+            first->next=nextpair;
+            
+            if(prev==NULL){
+                head=second;
+                
+            }
+            else{
+                prev->next = second;
+            }
+            prev=first;
+            first=nextpair;
+        }
+            return head;
+    }
+};
